157d8cd2cb31c4bb77e697b91c3d8776c9bd33bd
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-08-15 12:47:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const byteSize = (str) => {
   // write your code here
+	let text = str;
+	let blob = new blob([text],{type:'text/plain'});
+	console.log(blob.size);
+	
+	
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+//const str = prompt("Enter some string.");
+//alert(byteSize(str));
